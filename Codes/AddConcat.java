f76c0fa573abbcff58aa403b0db29f6ca9aaa20a
prabhu kalyan <57695741+prabhu30@users.noreply.github.com>
2020-12-20 18:29:05 +0530

    Create AddConcat.java

---
+++ Codes/AddConcat.java
@@ -0,0 +1,16 @@
+public class AddConcat
+{
+    public static void main(String[] args)
+    {
+        System.out.println(2+0+1+6+"GeeksforGeeks");
+        System.out.println("GeeksforGeeks"+2+0+1+6);
+        System.out.println(2+0+1+5+"GeeksforGeeks"+2+0+1+6);
+        System.out.println(2+0+1+5+"GeeksforGeeks"+(2+0+1+6));
+    }
+}
+
+//  OUTPUT :
+//  9GeeksforGeeks
+//  GeeksforGeeks2016
+//  8GeeksforGeeks2016
+//  8GeeksforGeeks9
