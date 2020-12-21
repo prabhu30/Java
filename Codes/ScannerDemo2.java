36109f172ddbb105a335e8f9174291161c1f4ba6
prabhu kalyan <57695741+prabhu30@users.noreply.github.com>
2020-12-21 08:16:41 +0530

    Create ScannerDemo2.java

---
+++ Codes/ScannerDemo2.java
@@ -0,0 +1,42 @@
+// Java program to read some values using Scanner
+// class and print their mean.
+import java.util.Scanner;
+
+public class ScannerDemo2
+{
+    public static void main(String[] args)
+    {
+        // Declare an object and initialize with
+        // predefined standard input object
+        Scanner sc = new Scanner(System.in);
+
+        // Initialize sum and count of input elements
+        int sum = 0, count = 0;
+
+        // Check if an int value is available
+        while (sc.hasNextInt())
+        {
+            // Read an int value
+            int num = sc.nextInt();
+            sum += num;
+            count++;
+        }
+        int mean = sum / count;
+        System.out.println("Mean: " + mean);
+    }
+}
+
+
+
+/*
+INPUT :
+10
+10
+10
+10
+A
+80
+
+OUTPUT :
+Mean: 10
+*/
